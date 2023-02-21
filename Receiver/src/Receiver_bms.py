1772155b7f8f5494d3d7a755a79fdf4328838dcc
Singh Om Prakash (MS/ESX8-INT-XC) <iom2kor@bosch.com>
2023-02-22 00:06:48 +0530

    Adding min max of streaming data

--- Receiver/src/Receiver_bms.py
+++ Receiver/src/Receiver_bms.py
@@ -24,12 +24,30 @@ class ReceiverBMS:
             for idx, key in enumerate(battery_param):
                 battery_param[key] = re.search(self.regex_match, console_data).group(idx+1)
         return battery_param
+    
+    def min_max_value(self, val_list):
+        return "Min : {} Max : {}".format(min(val_list), max(val_list))
 
+    def print_min_max_string(self, param_dict):
+        global Asorted_Battery_Parameter
+        for param, value in param_dict.items():
+            Asorted_Battery_Parameter[param].append(value)
+            param_dict[param] = self.min_max_value(Asorted_Battery_Parameter[param])
+        return param_dict
+    
 if __name__ == "__main__":
+    
     R_obj = ReceiverBMS()
+
+    # This is to list all the min max average as per dynamic count of parameter
+    global Asorted_Battery_Parameter
+    Asorted_Battery_Parameter = R_obj.get_json_data(R_obj.receiver_config_json)["Receiver_Config"]["Battery_Parameters"]
+    
     for i in range(R_obj.stream_length):
         console_sender_data = input()
         param_dict = R_obj.get_battery_parameters(console_sender_data)
+        print(console_sender_data, " | ", json.dumps(R_obj.print_min_max_string(param_dict)).replace("{","").replace("}",""))
+        
         
         
         
